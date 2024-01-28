36f3aed059753e8dd6af05625789ec0aabe28069
AndrewJoji <andrewjoji71@gmail.com>
2024-01-27 21:29:00 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,23 +11,23 @@ const Page: React.FC<PageProps> = () => {
   return (
     <>
       <Header />
-
+  
       <div className="container">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
           <AboutMe />
           <ContactInformation />
         </div>
-
+{/*     
         <div className="mt-12">
           <Skills />
-        </div>
-
-        <div className="mt-12">
-          <Professional />
-        </div>
-
+        </div> */}
+        
         <div className="mt-12">
           <Achievements />
+        </div>
+        
+        <div className="mt-12">
+          <Professional />
         </div>
 
         <div className="mt-12">
