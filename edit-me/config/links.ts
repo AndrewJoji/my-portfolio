3e085e6739f56b8af50afdfc6b502ea1641c12e4
AndrewJoji <andrewjoji71@gmail.com>
2024-01-27 22:01:27 -0800

    Update links.ts

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -7,7 +7,7 @@ import {
 
 import {
   faEnvelopeOpen,
-} from '@fortawesome/free-regular-svg-icons';
+} from '@fortawesome/free-solid-svg-icons';
 
 import { CMSLink } from 'edit-me/types/CMSLink';
 
